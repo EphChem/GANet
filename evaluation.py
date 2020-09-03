f5f4781daeefed26f33a5b8d2d8a4f5aead1e06d
Ephrem Chemali <ephrem.chemali@gmail.com>
2020-09-03 16:48:40 -0400

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -176,9 +176,9 @@ if __name__ == "__main__":
         if opt.kitti2015:
             leftname = file_path + 'image_02/data/' + current_file[0: len(current_file) - 1]
             rightname = file_path + 'image_03/data/' + current_file[0: len(current_file) - 1]
-            dispname = file_path + 'disparity_gt/' + current_file[0: len(current_file) - 1].split('.')[0] + '.npy'
-            disp = np.load(dispname)
-            disp = np.asarray(disp) # / 256.0
+            dispname = file_path + 'disparity_gt/' + current_file[0: len(current_file) - 1]
+            disp = Image.open(dispname)
+            disp = np.asarray(disp) / 256.0
             savename = opt.save_path + current_file[0: len(current_file) - 1]
         elif opt.kitti:
             leftname = file_path + 'colored_0/' + current_file[0: len(current_file) - 1]
